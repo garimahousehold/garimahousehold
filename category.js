23874989e087e08edb3906f4b83d13eebf97df5a
garimahousehold <garimakothari1995@gmail.com>
2026-08-09 23:52:52 +0530

    update

--- category.js
+++ category.js
@@ -3,12 +3,11 @@
 // CATEGORY.JS - FINAL
 // ==========================================
 
-import { db } from "./firebase.js";
-
 import {
+    db,
     collection,
     getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from "./firebase.js";
 
 
 // ==========================================
